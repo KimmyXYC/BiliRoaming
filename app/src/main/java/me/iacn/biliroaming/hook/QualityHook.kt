090d6a39db2afd52c5b1367fce5cfff8326201f6
KimmyXYC <kimmyxyc@163.com>
2023-04-08 23:11:27 +0800

    feat: Force Enable Vip Quality

--- app/src/main/java/me/iacn/biliroaming/hook/QualityHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/QualityHook.kt
@@ -6,10 +6,22 @@ import me.iacn.biliroaming.utils.sPrefs
 
 class QualityHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
-        sPrefs.getString("cn_server_accessKey", null) ?: return
+        if (!sPrefs.getBoolean("enable_vip_quality", false)
+            && sPrefs.getString("cn_server_accessKey", null) == null)
+        ) return
         Log.d("startHook: Quality")
 
         "com.bilibili.lib.accountinfo.model.VipUserInfo".hookBeforeMethod(
+            mClassLoader,
+            "isEffectiveVip"
+        ) {
+            Thread.currentThread().stackTrace.find { stack ->
+                stack.className.contains(".quality.")
+            } ?: return@hookBeforeMethod
+            it.result = true
+        }
+
+        "com.bilibili.lib.accountinfo.model.VipExtraUserInfo".hookBeforeMethod(
             mClassLoader,
             "isEffectiveVip"
         ) {
