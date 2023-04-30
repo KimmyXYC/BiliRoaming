cfb0d83cfe2724f06066167eeeb0da71b977fb90
エルフです <kimmyxyc@163.com>
2023-04-30 10:50:12 +0800

    feat: Allow remember lossless setting switch state

--- app/src/main/java/me/iacn/biliroaming/XposedInit.kt
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.kt
@@ -112,6 +112,7 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
                     startHook(PlaybackSpeedHook(lpparam.classLoader))
                     startHook(TextFoldHook(lpparam.classLoader))
                     startHook(ScreenOrientationHook(lpparam.classLoader))
+                    startHook(LosslessSettingHook(lpparam.classLoader))
                     startHook(DynamicHook(lpparam.classLoader))
                     startHook(ProtoBufHook(lpparam.classLoader))
                     startHook(PlayArcConfHook(lpparam.classLoader))
