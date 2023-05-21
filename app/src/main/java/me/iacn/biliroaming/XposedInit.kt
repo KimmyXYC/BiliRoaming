b675dd6a53fd6cbd7e1b0b90d9eeb851fb739e58
エルフです <69386744+KimmyXYC@users.noreply.github.com>
2023-05-21 05:51:54 +0000

    feat: Disable auto subscribe

--- app/src/main/java/me/iacn/biliroaming/XposedInit.kt
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.kt
@@ -104,6 +104,7 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
                     startHook(QualityHook(lpparam.classLoader))
                     startHook(SubtitleDownloadHook(lpparam.classLoader))
                     startHook(ChannelTabUIHook(lpparam.classLoader))
+                    startHook(FavFolderDialogHook(lpparam.classLoader))
                     startHook(PlaybackSpeedHook(lpparam.classLoader))
                     startHook(TextFoldHook(lpparam.classLoader))
                     startHook(ScreenOrientationHook(lpparam.classLoader))
