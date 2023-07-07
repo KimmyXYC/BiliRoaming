12bc274675457c2c32514a998bfbd78c720df17a
エルフです <69386744+KimmyXYC@users.noreply.github.com>
2023-07-07 13:22:01 +0000

    fix: Adapt upstream changes

--- app/src/main/java/me/iacn/biliroaming/XposedInit.kt
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.kt
@@ -134,7 +134,7 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
                     startHook(VideoQualityHook(lpparam.classLoader))
                     startHook(PublishToFollowingHook(lpparam.classLoader))
                     startHook(UposReplaceHook(lpparam.classLoader))
-                    startHook(SpeedHook(lpparam.classLoader))
+                    // startHook(SpeedHook(lpparam.classLoader))
                 }
 
                 lpparam.processName.endsWith(":web") -> {
